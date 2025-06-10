1c7e79464509f823c5f66781a7408231db6340aa
adrianowerneck <adrianowerneck@id.uff.br>
2025-06-10 10:53:37 -0300

    Added queue system to pass data between tasks safely

---
+++ src/queues.cpp
@@ -0,0 +1,37 @@
+#include "queues.hpp"
+#include "freertos/FreeRTOS.h"
+
+QueueHandle_t message_queue;
+QueueHandle_t main_radio_queue;
+QueueHandle_t auxiliary_radio_queue;
+QueueHandle_t internet_queue;
+QueueHandle_t logger_queue;
+QueueHandle_t can_queue; 
+
+#define message_queue_length 10
+#define main_radio_queue_length 10
+#define auxiliary_radio_queue_length 10
+#define internet_queue_length 10
+#define logger_queue_length 10
+#define can_queue_length 10
+
+void initialize_queues() {
+    message_queue = xQueueCreate(message_queue_length, sizeof(message_t));
+    main_radio_queue = xQueueCreate(main_radio_queue_length, sizeof(message_t));
+    auxiliary_radio_queue = xQueueCreate(auxiliary_radio_queue_length, sizeof(message_t));
+    internet_queue = xQueueCreate(internet_queue_length, sizeof(message_t));
+    logger_queue = xQueueCreate(logger_queue_length, sizeof(message_t));
+    can_queue = xQueueCreate(can_queue_length, sizeof(message_t));
+
+    if (message_queue == NULL || main_radio_queue == NULL || auxiliary_radio_queue == NULL ||
+        internet_queue == NULL || logger_queue == NULL || can_queue == NULL) {
+        printf("Failed to create one or more queues.\n");
+        //Halt the system
+        while (true) {
+            vTaskDelay(pdMS_TO_TICKS(1000)); // Wait indefinitely
+        }
+    }
+}
+
+
+
